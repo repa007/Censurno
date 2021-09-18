992ce673aeea5ec53e9727542c2cd025bf4dc973
repa007 <50541383+repa007@users.noreply.github.com>
2021-09-18 10:52:40 +0300

    Update vtoroe.cpp

--- Lab1/vtoroe.cpp
+++ Lab1/vtoroe.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+
+	//объявляем переменные
 	int a = 6;
-	int b = 3;
-	int c = 471;
-	int d = 0;
-	cout << "print integer\n";
-	cin >> d;
-	int Result = 0;
-	Result = a + c ^ b - d; //сначала возведение в степень
-	cout << "\n\n(>^.^)>  " << Result << endl << endl << endl << endl;
+	float b = 16.565;
+	string c = "5.3";
+	
+
+	//преобразуем типы...
+	a = double(a);
+	b = double(b);
+	double cc = stod(c); //зафигачили строку в дабл
+	double Result = a + cc / b;
+
+	cout << "(>^.^)> " << Result << endl << endl << endl;
 }
